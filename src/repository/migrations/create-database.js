dc24000b629a86917911c0b94136ca1001198bdd
roncevickresimir <73893906+roncevickresimir@users.noreply.github.com>
2022-09-20 12:34:33 +0200

    migrationsCleanup

---
+++ src/repository/migrations/create-database.js
@@ -0,0 +1,559 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    //Support.sequelize.query('CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public', {raw: true});
+    try {
+      await queryInterface.createTable(
+        'Account',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          name: {
+            field: 'name',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false,
+            unique: true,
+          },
+          abrv: {
+            field: 'abrv',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+            unique: true,
+          },
+        },
+        { transaction }
+      );
+
+      await queryInterface.createTable(
+        'Category',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          name: {
+            field: 'name',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false,
+            unique: true,
+          },
+          abrv: {
+            field: 'abrv',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+            unique: true,
+          },
+        },
+        { transaction }
+      );
+
+      
+      await queryInterface.createTable(
+        'EndUser',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          userName: {
+            field: 'username',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+          email: {
+            field: 'email',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+          pasword: {
+            field: 'pasword',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+            unique: false,
+          },
+        },
+        { transaction }
+      );
+      
+      await queryInterface.createTable(
+        'EndUserQuest',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          questId: {
+            field: 'questId',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+          },
+          userId: {
+            field: 'userId',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+          },
+          started: {
+            field: 'started',
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            unique: false,
+            defaultValue: false,
+          },
+          complete: {
+            field: 'complete',
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            unique: false,
+            defaultValue: false,
+          },
+        },
+        { transaction }
+      );
+      
+      await queryInterface.createTable(
+        'EndUserStation',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          stationId: {
+            field: 'stationId',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+          },
+          userId: {
+            field: 'userId',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+          },
+          complete: {
+            field: 'complete',
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            unique: false,
+            defaultValue: false,
+          },
+        },
+        { transaction }
+      );
+
+      await queryInterface.createTable(
+        'Image',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          stationId: {
+            field: 'stationId',
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            unique: false,
+          },
+          fileName: {
+            field: 'fileName',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+          },
+          fileExt: {
+            field: 'fileExt',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+          },
+          filePath: {
+            field: 'filePath',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+          },
+          createdAt: {
+            field: 'createdAt',
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+        },
+        { transaction }
+      );
+
+      await queryInterface.createTable(
+        'QRCode_Image',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          fileName: {
+            field: 'fileName',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+          },
+          fileExt: {
+            field: 'fileExt',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+          },
+          filePath: {
+            field: 'filePath',
+            type: Sequelize.DataTypes.CITEXT,
+            allowNull: false,
+          },
+          createdAt: {
+            field: 'createdAt',
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+          stationId: {
+            field: 'stationId',
+            type: Sequelize.UUID,
+            allowNull: false,
+            unique: false,
+            defaultValue: Sequelize.literal(
+              'uuid_in((md5((random())::text))::cstring)'
+            ),
+          }
+        },
+        { transaction }
+      );
+      
+      await queryInterface.createTable(
+        'Quest',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          title: {
+            field: 'title',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false,
+            unique: false,
+          },
+          description: {
+            field: 'description',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: true,
+            unique: false,
+          },
+          categoryIds: {
+            field: 'categoryIds',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false,
+            unique: false,
+          },
+          userId: {
+            field: 'userId',
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            unique: false,
+          },
+          latitude: {
+            field: 'latitude',
+            type: Sequelize.DOUBLE,
+            allowNull: false,
+            unique: false,
+            defaultValue: 0,
+          },
+          longitude: {
+            field: 'longitude',
+            type: Sequelize.DOUBLE,
+            allowNull: false,
+            unique: false,
+            defaultValue: 0,
+          },
+        },
+        { transaction }
+      );
+
+      await queryInterface.createTable(
+        'Reward',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          userId: {
+            field: 'userId',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+          },
+          rewardTypeId: {
+            field: 'rewardTypeId',
+            type: Sequelize.UUID,
+            allowNull: false,
+            unique: false,
+          },
+        },
+        { transaction }
+      );
+      
+      await queryInterface.createTable(
+        'RewardType',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          name: {
+            field: 'name',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false,
+            unique: false,
+          },
+          description: {
+            field: 'description',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: true,
+            unique: false,
+          },
+          stationId: {
+            field: 'stationId',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+          },
+          userId: {
+            field: 'userId',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+          },
+          image: {
+            field: 'image',
+            type: Sequelize.CITEXT,
+            allowNull: true,
+            unique: false,
+          },
+        },
+        { transaction }
+      );
+      
+      await queryInterface.createTable(
+        'Role',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          name: {
+            field: 'name',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+          abrv: {
+            field: 'abrv',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+        },
+        { transaction }
+      );
+
+      await queryInterface.createTable(
+        'Station',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          questId: {
+            field: 'questId',
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            unique: false,
+          },
+          title: {
+            field: 'title',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false,
+            unique: false,
+          },
+          description: {
+            field: 'description',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: true,
+            unique: false,
+          },
+          categoryIds: {
+            field: 'categoryIds',
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false,
+            unique: false,
+          },
+          accountId: {
+            field: 'accountId',
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            unique: false,
+          },
+          latitude: {
+            field: 'latitude',
+            type: Sequelize.DOUBLE,
+            allowNull: false,
+            unique: false,
+            defaultValue: 0,
+          },
+          longitude: {
+            field: 'longitude',
+            type: Sequelize.DOUBLE,
+            allowNull: false,
+            unique: false,
+            defaultValue: 0,
+          },
+          userId:{
+            field: 'userId',
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            unique: false,
+          },
+        },
+        { transaction }
+      );
+      
+      await queryInterface.createTable(
+        'Station_Quest_relation',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          stationId: {
+            field: 'stationId',
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            unique: false,
+          },
+          questId: {
+            field: 'questId',
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            unique: false,
+          },
+        },
+        { transaction }
+      );
+
+      await queryInterface.createTable(
+        'User',
+        {
+          id: {
+            field: 'id',
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          userName: {
+            field: 'userName',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+          firstName: {
+            field: 'firstName',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+          lastName: {
+            field: 'lastName',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+          email: {
+            field: 'email',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+          },
+          roleId: {
+            field: 'roleId',
+            type: Sequelize.UUID,
+            allowNull: false,
+          },
+          password: {
+            field: 'password',
+            type: Sequelize.CITEXT,
+            allowNull: false,
+            unique: false,
+          },
+        },
+        { transaction }
+      );
+
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.dropTable('Account', { transaction });
+      await queryInterface.dropTable('Category', { transaction });
+      await queryInterface.dropTable('EndUser', { transaction });
+      await queryInterface.dropTable('EndUserQuest', { transaction });
+      await queryInterface.dropTable('EndUserStation', { transaction });
+      await queryInterface.dropTable('Image', { transaction });
+      await queryInterface.dropTable('QRCode_Image', { transaction });
+      await queryInterface.dropTable('Quest', { transaction });
+      await queryInterface.dropTable('Reward', { transaction });
+      await queryInterface.dropTable('RewardType', { transaction });
+      await queryInterface.dropTable('Role', { transaction });
+      await queryInterface.dropTable('Station', { transaction });
+      await queryInterface.dropTable('Station_Quest_relation', { transaction });
+      await queryInterface.dropTable('User', { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
--- src/repository/migrations/insert-base-data.js
+++ src/repository/migrations/insert-base-data.js
@@ -38,7 +38,6 @@ module.exports = {
         { transaction }
       );
 
-
       await queryInterface.bulkInsert(
         'User',
         [
@@ -47,14 +46,75 @@ module.exports = {
             userName: 'SuperAdmin',
             firstName: 'Super',
             lastName: 'Admin',
-            email: 'neven@koona.tech',
+            email: 'admin@avantourist.eu',
             password: '$2y$10$5OLgHxOtUqj0xsdJRUbRQuUtdvM6ol5BM7egBDPf8ZAPxF79VL/ru',
             roleId: idAdmin,
           },
         ],
         { transaction }
       );
+      
+      await queryInterface.bulkInsert(
+        'Account',
+        [
+          {
+            id: v4(),
+            abrv: 'free',
+            name: 'Free',
+          },
+          {
+            id: v4(),
+            abrv: 'premium',
+            name: 'Premium',
+          },
+          {
+            id: v4(),
+            abrv: 'premium-plus',
+            name: 'Premiun plus',
+          },
+        ],
+        { transaction }
+      );
 
+      await queryInterface.bulkInsert(
+        'Category',
+        [
+          {
+            id: v4(),
+            abrv: 'gastro',
+            name: 'Gastro',
+          }, {
+            id: v4(),
+            abrv: 'nature',
+            name: 'Nature',
+          }, {
+            id: v4(),
+            abrv: 'adrenaline',
+            name: 'Adrenaline',
+          }, {
+            id: v4(),
+            abrv: 'beach',
+            name: 'Beach',
+          }, {
+            id: v4(),
+            abrv: '18+',
+            name: '18+',
+          }, {
+            id: v4(),
+            abrv: 'party',
+            name: 'Party',
+          }, {
+            id: v4(),
+            abrv: 'teen',
+            name: 'Teen',
+          }, {
+            id: v4(),
+            abrv: 'family',
+            name: 'Family',
+          },
+        ],
+      );
+      
       await transaction.commit();
     } catch (err) {
       await transaction.rollback();
